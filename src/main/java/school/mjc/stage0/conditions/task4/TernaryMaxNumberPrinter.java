072745df740e75b146c97835326b9660646efe86
Vu <nguyentuanvu9x@gmail.com>
2023-08-18 10:31:24 +0300

    Adding: TernaryMaxNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = first > second && first > third ?
+                first:
+                (second > third ?
+                        second:
+                        third);
+        System.out.println(result);
     }
 }
